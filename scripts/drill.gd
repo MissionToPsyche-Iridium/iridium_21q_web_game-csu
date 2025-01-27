41133ddf4576f6a50bf9be71c1e4dcd8e91032de
MWSystems <wasserbauermaxwell@gmail.com>
2025-01-27 18:41:12 -0500

    Small Edit to make code run.

--- scripts/drill.gd
+++ scripts/drill.gd
@@ -7,15 +7,20 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
+		
 		var tilemapcords = body.local_to_map(body.to_local(to_global(position)))
-		tilemapcords.y += 1
-		if tilemapcords.x < 0:
-			tilemapcords.x += 1
-		print(tilemapcords)
+		print("LOCAL POS:", position)
+		print("LOCAL TO MAP:", body.local_to_map(position))
+		print("GLOBAL TO MAP:", body.local_to_map(global_position))
+		print("GLOBAL: ", global_position)
+		#tilemapcords.y += 1
+		#if tilemapcords.x < 0:
+		#	tilemapcords.x += 1
+		print("TILEMAPCORDS: ", tilemapcords)
 		body.set_cell(tilemapcords, 0, Vector2(1,0))
 		body.erase_cell(tilemapcords)
 		print("DOWN")
-	pass # Replace with function body.
+		pass # Replace with function body.
 func _on_body_exited(body: Node2D) -> void:
 	print("LEAVE")
 	pass 
