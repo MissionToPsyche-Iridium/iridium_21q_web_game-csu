49da0e98b78013d4c6ca939cd4844dc0a34d0eda
MWSystems <wasserbauermaxwell@gmail.com>
2025-03-07 20:18:10 -0500

    Added EventBus

--- scripts/EventBus.gd
+++ scripts/EventBus.gd
@@ -4,3 +4,12 @@ extends Node
 #Everything that "Triggers" off of other objects comes here.
 
 #TODO: move other triggers, CurrMgr and Inventory Triggers into this global.
+
+#GAMEPLAY RELATED
+signal tile_broken() #When a tile breaks.
+signal pickup_dropped() #When a pickup is dropped.
+
+
+#SHOP RELATED
+signal item_bought() #When the player buys an item.
+signal on_reroll() #When the player rerolls.
