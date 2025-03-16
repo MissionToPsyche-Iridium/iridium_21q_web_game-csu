16f0c16e81479ae9b751db0ec23a6d87b068d208
Imanyy28 <162731123+Imanyy28@users.noreply.github.com>
2025-03-16 05:23:09 -0400

    audio effect

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -4,15 +4,15 @@ extends Control
 @onready var row: HBoxContainer = $row
 @onready var parallax_layer = $ParallaxBackground/ParallaxLayer  
 @onready var parallax_background = $ParallaxBackground  
+@onready var background_sound = $background_sound
 
 func _ready() -> void:
+	background_sound.play()  
 	for display in row.get_children():
 		if display.name != "BADOP" and display is SubViewportContainer:  
 			displays.append(display)
 
-	# ✅ Ensure Infinite Looping (Updated for 816x480)
 	parallax_layer.motion_mirroring = Vector2(816, 0)  # Image width now 816px
 
 func _process(delta):
-	
-		parallax_background.scroll_offset.x -= 50 * delta
+	parallax_background.scroll_offset.x -= 50 * delta
