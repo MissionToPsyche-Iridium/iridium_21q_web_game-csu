18995c5093a94d0ecbd2e393843e4a778c7413c6
Imanyy28 <162731123+Imanyy28@users.noreply.github.com>
2025-03-16 04:38:27 -0400

    Added parallax space background

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -2,18 +2,17 @@ extends Control
 
 @onready var displays: Array = []
 @onready var row: HBoxContainer = $row
+@onready var parallax_layer = $ParallaxBackground/ParallaxLayer  
+@onready var parallax_background = $ParallaxBackground  
 
 func _ready() -> void:
 	for display in row.get_children():
-		if display.name != "BADOP" and display is SubViewportContainer:	 #Remove the "Dud" Display to fix the juice bug.
+		if display.name != "BADOP" and display is SubViewportContainer:  
 			displays.append(display)
 
+	# ✅ Ensure Infinite Looping (Updated for 816x480)
+	parallax_layer.motion_mirroring = Vector2(816, 0)  # Image width now 816px
 
-func _on_reroll_pressed() -> void:
-	#Ask all displays to reroll there item.
-	#TODO add active system and make it reroll.
-	#TODO make reroll respect credits.
-	for display in displays:
-		display.display.display_item(Itemdict.get_random_item_passive())
-		display.on_reroll()
-	pass # Replace with function body.
+func _process(delta):
+	
+		parallax_background.scroll_offset.x -= 50 * delta
