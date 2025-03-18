c5022ceb03af0896e00b7343b966bede24f8a921
MWSystems <wasserbauermaxwell@gmail.com>
2025-03-17 22:32:34 -0400

    Fix reroll bug

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -16,3 +16,12 @@ func _ready() -> void:
 
 func _process(delta):
 	parallax_background.scroll_offset.x -= 50 * delta
+
+
+func _on_reroll_pressed() -> void:
+	#Ask all displays to reroll there item.
+	#TODO add active system and make it reroll.
+	#TODO make reroll respect credits.
+	for display in displays:
+		display.display.display_item(Itemdict.get_random_item_passive())
+		display.on_reroll()
